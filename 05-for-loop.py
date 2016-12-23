fbea67dac52a4d792553313834d2abd26e0e3b04
“senaaac” <listen.and.listen@gmail.com>
2016-12-23 21:25:08 +0900

    Add 05-for-loop.py
    -for, range, while

---
+++ 05-for-loop.py
@@ -0,0 +1,16 @@
+# for, range
+# while
+
+# for
+# for num in range(1, 10): # slice와 동일하게 마지막 인덱스는 표시하지 않는다
+#     print(num)
+#
+# num_list = [1,2,3,4,5,6,7,8,9]
+# for num in num_list:
+#     print(num)
+
+# while : false가 나올때까지 무한 루프가 됨
+a = 1
+while a < 10:
+    print(a)
+    a = a + 1
